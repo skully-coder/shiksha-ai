54f2462913fbb1b043071c855be525ec51aec2fc
Z33xD <zeeshanmoinshariff@gmail.com>
2025-10-08 18:10:16 +0530

    Changed background colour of the toast notification to be white

--- src/components/nav-items.tsx
+++ src/components/nav-items.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -37,8 +36,10 @@ export function NavItems() {
       }
       router.push('/login');
       toast({
+          variant: 'success',
           title: "Logged Out",
-          description: "You have been successfully logged out."
+          description: "You have been successfully logged out.",
+          style: { backgroundColor: "#fff", color: "#222" } // Set background to white, text to dark
       });
     } catch (error) {
       console.error("Error signing out: ", error);

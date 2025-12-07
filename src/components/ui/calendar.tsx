b55e5615fd363ef938704887ffb2218dac3b23a2
Abhinav Agrawal <abhinav.hfs@gmail.com>
2025-12-07 11:11:30 +0530

    feat: Upgrade Genkit, Next.js, and React dependencies, and update calendar component and tsconfig.

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,11 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        Chevron: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
       {...props}
